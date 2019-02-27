48c8a0ac59d4b07e52c9b7d16b891290261477c7
Ivan Krivov <vnk729@gmail.com>
2019-02-27 18:00:00 +0300

    add brainEven

--- src/index.js
+++ src/index.js
@@ -6,4 +6,29 @@ const greeting = () => {
   console.log(`Hello, ${userName}!`);
 };
 
-export default greeting;
+const isEven = num => (num % 2 === 0 ? 'yes' : 'no');
+
+const brainEven = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no". \n');
+
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}! \n`);
+
+  for (let i = 0; i < 3; i += 1) {
+    const random = Math.floor(Math.random() * 100);
+    console.log(`Question: ${random}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    if (isEven(random) === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven(random)}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export { greeting, brainEven };
