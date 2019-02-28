7708c9e1e0b69a38e581890cbf386287e2aa6765
Ivan Krivov <vnk729@gmail.com>
2019-02-28 15:38:33 +0300

    edit

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import { brainEven } from '..';
+import runGame from '../games/even';
 
-brainEven();
+runGame();
--- src/index.js
+++ src/index.js
@@ -6,29 +6,10 @@ const greeting = () => {
   console.log(`Hello, ${userName}!`);
 };
 
-const isEven = num => (num % 2 === 0 ? 'yes' : 'no');
+const numRound = 3;
+const isEven = num => num % 2 === 0;
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-const brainEven = () => {
-  console.log('Welcome to the Brain Games!');
-  console.log('Answer "yes" if number even otherwise answer "no". \n');
-
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}! \n`);
-
-  for (let i = 0; i < 3; i += 1) {
-    const random = Math.floor(Math.random() * 100);
-    console.log(`Question: ${random}`);
-    const answer = readlineSync.question('Your answer: ');
-
-    if (isEven(random) === answer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven(random)}'.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
+export {
+  greeting, numRound, isEven, randomInt,
 };
-
-export { greeting, brainEven };
